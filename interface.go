639e4b5f04afad8956fa2ebf09493f9ad78f45cd
bruk-lemma <bruklemma2017@gmail.com>
2023-11-11 17:20:55 +0300

    interface fifnished

--- interface.go
+++ interface.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	typeThatSatisfiesMultipleInterfaces()
+	InterfaceWithSimilarMethods()
 }
 
 //	func createInterface() {
@@ -46,4 +47,47 @@ func typeThatSatisfiesMultipleInterfaces() {
 	// Type assertion to access Pentagon-specific methods
 	var pent Pentagon = obj.(Pentagon)
 	pent.Perimeter()
+}
+
+type Car string
+
+func (c Car) Structure() []string {
+	var parts = []string{"ECU", "Engine", "Air Filters", "Wipers", "Gas Task"}
+	return parts
+}
+
+func (c Car) Speed() string {
+	return "200 Km/Hrs"
+}
+
+type Man string
+
+func (m Man) Structure() []string {
+	var parts = []string{"Brain", "Heart", "Nose", "Eyelashes", "Stomach"}
+	return parts
+}
+
+func (m Man) Performance() string {
+	return "8 Hrs/Day"
+}
+func InterfaceWithSimilarMethods() {
+
+	type Vehicle interface {
+		Structure() []string // Common Method
+		Speed() string
+	}
+
+	type Human interface {
+		Structure() []string // Common Method
+		Performance() string
+	}
+	var bmw Vehicle
+	bmw = Car("World Top Brand")
+
+	var labour Human
+	labour = Man("Software Developer")
+
+	for i, j := range bmw.Structure() {
+		fmt.Printf("%-15s <=====> %15s\n", j, labour.Structure()[i])
+	}
 }

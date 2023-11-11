96246ff470e10db510faa03e6329106cd98f4bf1
bruk-lemma <bruklemma2017@gmail.com>
2023-11-11 18:35:33 +0300

    using channels to pass values in go routines

--- go_routines.go
+++ go_routines.go
@@ -15,16 +15,29 @@ func main() {
 	// go create_go_routine("https://stackoverflow.com")
 	// time.Sleep(10 * time.Second)
 
-	fmt.Println("Using WaitGroup")
-	// Add a count of three, one for each goroutine.
-	wg.Add(3)
-	go using_WaitGroup_in_go_routines("https://www.golangprograms.com")
-	go using_WaitGroup_in_go_routines("https://coderwall.com")
-	go using_WaitGroup_in_go_routines("https://stackoverflow.com")
+	//-----------------------------------------------------
 
-	// Wait for the goroutines to finish.
+	// fmt.Println("Using WaitGroup")
+	// // Add a count of three, one for each goroutine.
+	// wg.Add(3)
+	// go using_WaitGroup_in_go_routines("https://www.golangprograms.com")
+	// go using_WaitGroup_in_go_routines("https://coderwall.com")
+	// go using_WaitGroup_in_go_routines("https://stackoverflow.com")
+
+	// // Wait for the goroutines to finish.
+	// wg.Wait()
+	// fmt.Println("Terminating Program")
+
+	//----------------------------------------------------
+
+	fmt.Println("Using chaneels to send values")
+
+	nums := make(chan int) //declare unbuffred channeal
+	wg.Add(1)
+	go fetchValuesFromGoroutines("https://www.golangprograms.com", nums)
+	fmt.Println(<-nums) //Read the values from unbufferd channeal
 	wg.Wait()
-	fmt.Println("Terminating Program")
+	close(nums) //close the channel
 
 }
 
@@ -64,5 +77,25 @@ func using_WaitGroup_in_go_routines(url string) {
 		log.Fatal(err)
 	}
 	fmt.Println("Step4 :", len(body))
+
+}
+
+func fetchValuesFromGoroutines(url string, nums chan int) {
+	//schedule the call to wait group;s Done to tell go routine  is complete
+	defer wg.Done()
+
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//send value to the unbuffered channel
+	nums <- len(body)
 
 }

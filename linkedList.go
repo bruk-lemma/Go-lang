9354f44625400ec1053fdfa42ce112dfa40b6b03
bruk-lemma <bruklemma2017@gmail.com>
2023-11-14 16:46:40 +0300

    created insert func for the double-linked-list function

--- linkedList.go
+++ linkedList.go
@@ -9,21 +9,29 @@ func main() {
 	list.add(3)
 	list.add(4)
 
-	fmt.Println("Initial List: ")
-	printList(list)
+	// fmt.Println("Initial List: ")
+	// printList(list)
 
-	list.remove(2)
-	fmt.Println("List after removing 2: ")
-	printList(list)
+	// list.remove(2)
+	// fmt.Println("List after removing 2: ")
+	// printList(list)
 
-	list.remove(4)
-	fmt.Println("List after removing 4: ")
+	// list.remove(4)
+	// fmt.Println("List after removing 4: ")
+	// printList(list)
+
+	fmt.Println("Initial List before insert: ")
+	printList(list)
+	fmt.Println("Initial List after insert: ")
+
+	list.Insert(9, 1)
 	printList(list)
 }
 
 type Node struct {
 	data int
 	next *Node
+	prev *Node
 }
 
 type List struct {
@@ -40,6 +48,34 @@ func (l *List) add(value int) {
 	curr := l.head
 	for curr.next != nil {
 		curr = curr.next
+	}
+	curr.next = newNode
+}
+
+func (l *List) Insert(value int, index int) {
+	newNode := &Node{data: value}
+
+	if l.head == nil || index == 0 {
+		newNode.next = l.head
+		if l.head != nil {
+			l.head.prev = newNode
+		}
+		l.head = newNode
+		return
+	}
+
+	curr := l.head
+	for i := 0; i < index-1; i++ {
+		if curr.next == nil {
+			break
+		}
+		curr = curr.next
+	}
+
+	newNode.next = curr.next
+	newNode.prev = curr
+	if curr.next != nil {
+		curr.next.prev = newNode
 	}
 	curr.next = newNode
 }

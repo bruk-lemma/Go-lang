e4ab2cb841741f8f3211afc73a6b9aa5d66904d5
bruk-lemma <bruklemma2017@gmail.com>
2023-11-23 11:06:06 +0300

    func to write into xml file uising a struct as format

--- reading_and_writing_diffrent_file.go
+++ reading_and_writing_diffrent_file.go
@@ -24,4 +24,17 @@ func main() {
 	fmt.Println(note.From)
 	fmt.Println(note.Heading)
 	fmt.Println(note.Body)
+	write_xml_file()
+}
+
+func write_xml_file() {
+
+	note := Notes{To: "Nicky",
+		From:    "Rock",
+		Heading: "Meeting",
+		Body:    "Meeting at 5pm!",
+	}
+	file, _ := xml.MarshalIndent(note, "", "")
+
+	_ = ioutil.WriteFile("notes1.xml", file, 0644)
 }

8f4b24dec6aa6b3d0b6dc0583fa53ee8fc9d524c
bruk-lemma <bruklemma2017@gmail.com>
2023-11-23 12:24:38 +0300

    get PTR and NS

--- DNS.go
+++ DNS.go
@@ -18,4 +18,16 @@ func main() {
 	cname, _ := net.LookupCNAME("m.facebook.com")
 	fmt.Println(cname)
 
+	//PTR pointer
+	//These records provide the reverse binding from addresses to names
+	ptr, _ := net.LookupAddr("6.8.8.8")
+	for _, ptrvalue := range ptr {
+		fmt.Println(ptrvalue)
+	}
+
+	//NS nameserver
+	nameserver, _ := net.LookupNS("facebook.com")
+	for _, ns := range nameserver {
+		fmt.Println(ns)
+	}
 }
